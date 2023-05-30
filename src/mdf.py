c6780da082659975b0582e08c9973519e4dd6dca
mariarezende1313 <maria.rezende@qitech.com.br>
2023-05-30 17:18:12 -0300

    ⚡️ Improve performance
    Co-authored-by: MathTakaki <MathTakaki@users.noreply.github.com>

--- src/mdf.py
+++ src/mdf.py
@@ -4,26 +4,16 @@ import numpy as np
 
 class Fusca():
     def __init__(self) -> None:
-        self.V = 100/3.6  # m/s
+        self.V = 27.78  # m/s
         self.h_carro = 0.15
         self.L_carro = 3
-        self.d_dominio = 0.5 * self.L_carro
-        self.H_dominio = 2 * self.L_carro
+        self.d_dominio = 1.5
 
-        self.x_dominio = 2 * self.d_dominio + self.L_carro
-        self.y_dominio = self.H_dominio
+        self.x_dominio = 6
+        self.y_dominio = 6
 
         self.Nx = 100
         self.Ny = 100
-
-        # Create the grid
-        self.x = np.linspace(0, self.x_dominio, self.Nx)
-        self.y = np.linspace(0, self.y_dominio, self.Ny)
-
-        self.X, self.Y = np.meshgrid(self.x, self.y)
-
-        self.A = np.zeros((self.Nx*self.Ny, self.Nx*self.Ny))
-        self.b = np.zeros(self.Nx*self.Ny)
 
     def car_height(self, x):
         return np.sqrt(((self.L_carro/2)**2) - (x - self.d_dominio - (self.L_carro/2))**2) + self.h_carro
@@ -31,50 +21,63 @@ class Fusca():
     def index(self, i, j):
         return j * self.Nx + i
 
-    def mdf(self):
+    def setup_matrix(self):
+        # Define zero matrix with 2D linearization to 1D space
+        A = np.zeros((self.Nx*self.Ny, self.Nx*self.Ny))
+        b = np.zeros(self.Nx*self.Ny)
+
+        x = np.linspace(0, self.x_dominio, self.Nx)
+        y = np.linspace(0, self.y_dominio, self.Ny)
+
         for i in range(self.Nx):
             for j in range(self.Ny):
                 k = self.index(i, j)
                 if j == 0:
-                    self.A[k, k] = 1
-                    self.b[k] = 0
+                    A[k, k] = 1
+                    b[k] = 0
                 else:
-                    x_coord = self.x[i]
-                    y_coord = self.y[j]
+                    x_coord = x[i]
+                    y_coord = y[j]
                     if i == 0 or i == self.Nx - 1:
-                        self.A[k, k] = -1.0
-                        self.A[k, self.index(i - 1, j)] = 1.0
-                        self.b[k] = 0.0
+                        A[k, k] = -1.0
+                        A[k, self.index(i - 1, j)] = 1.0
+                        b[k] = 0.0
                     elif j == self.Ny - 1:
-                        self.A[k, k] = -3.0
-                        self.A[k, self.index(i, j - 1)] = 4.0
-                        self.b[k] = 4.0 * self.V * \
+                        A[k, k] = -3.0
+                        A[k, self.index(i, j - 1)] = 4.0
+                        b[k] = 4.0 * self.V * \
                             (1.0 / (self.y_dominio/self.Ny))
                     elif (0 <= y_coord <= self.car_height(x_coord)) and (self.d_dominio <= x_coord <= self.d_dominio + self.L_carro):
-                        self.A[k, k] = 1
-                        self.b[k] = 0
+                        A[k, k] = 1
+                        b[k] = 0
                     else:
-                        self.A[k, k] = -4.0
-                        self.A[k, self.index(i - 1, j)] = 1.0
-                        self.A[k, self.index(i + 1, j)] = 1.0
-                        self.A[k, self.index(i, j - 1)] = 1.0
-                        self.A[k, self.index(i, j + 1)] = 1.0
+                        A[k, k] = -4.0
+                        A[k, self.index(i - 1, j)] = 1.0
+                        A[k, self.index(i + 1, j)] = 1.0
+                        A[k, self.index(i, j - 1)] = 1.0
+                        A[k, self.index(i, j + 1)] = 1.0
 
-        T = np.linalg.solve(self.A, self.b)
-        T = T.reshape((self.Ny, self.Nx))
-
-        return T
-    
+        return A, b
 
     def plot(self):
 
-        T = self.mdf()
+        # Define meshgrid
+        x = np.linspace(0, self.x_dominio, self.Nx)
+        y = np.linspace(0, self.y_dominio, self.Ny)
+        X, Y = np.meshgrid(x, y)
+
+        # Init Setup matrix
+
+        T = self.setup_matrix()
+
         print(T)
-        plt.contour(self.X, self.Y, T, levels=20)
-        plt.colorbar(label='Temperature')
+
+        # Plot contour
+        plt.contour(X, Y, T, levels=20)
+        plt.colorbar(label='Phi')
         plt.xlabel('x')
         plt.ylabel('y')
-        plt.title('Temperature Distribution')
+        plt.title('Phi Distribution')
         plt.show()
 
 

34c8ead4a2da76114896f0f5fb62ff6e57558ca0
mariarezende1313 <maria.rezende@qitech.com.br>
2023-07-14 13:46:55 -0300

    Add POO organization

--- src/utils/mef_tools.py
+++ src/utils/mef_tools.py
@@ -1,41 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def rigidez_portico_matrix(E, A, L, I):
-
-    K = np.zeros(shape=(6, 6))
-    K[0, 0] = 1
-    K[0, 3] = -1
-    K[3, 0] = -1
-    K[3, 3] = 1
-
-    K *= (E * A) / L
-
-    K[1, :] = np.array([0, 12, 6*L, 0, -12, 6*L]) * ((E * I)/(L**3))
-
-    K[2, :] = np.array([0, 6*L, 4*(L**2), 0, -6*L, 2*(L**2)]
-                       ) * ((E * I)/(L**3))
-
-    K[4, :] = np.array([0, -12, -6*L, 0, 12, 6*L]) * ((E * I)/(L**3))
-
-    K[5, :] = np.array([0, 6*L, 2*(L**2), 0, -6*L,
-                       4*(L**2)]) * ((E * I)/(L**3))
-
-    return K
-
-
-def portico_rotation_matrix(theta):
-    T = np.zeros(shape=(6, 6))
-    theta = np.radians(theta)
-    T[0, :] = [np.cos(theta), np.sin(theta), 0, 0, 0, 0]
-    T[1, :] = [- np.sin(theta), np.cos(theta), 0, 0, 0, 0]
-    T[2, 2] = 1
-    T[3, :] = [0, 0, 0, np.cos(theta), np.sin(theta), 0]
-    T[4, :] = [0, 0, 0, -np.sin(theta), np.cos(theta), 0]
-    T[5, 5] = 1
-
-    return T
-
 
 def rigidez_trelica_matrix(E, A, L):
     K = np.zeros(shape=(6, 6))
@@ -61,62 +26,121 @@ def trelica_rotation_matrix(theta):
     return T
 
 
-def generate_dataframe_from_matrix(local_matrix, nodes):
-
-    column_names = []
-    index_names = []
-    for node in nodes:
-        u_dof = f"{node}_u"
-        v_dof = f"{node}_v"
-        phi_dof = f"{node}_phi"
-
-        column_names += [u_dof, v_dof, phi_dof]
-        index_names += [u_dof, v_dof, phi_dof]
-
-    matrix_df = pd.DataFrame(
-        data=local_matrix, columns=column_names, index=index_names)
-
-    return matrix_df
 
 
-def discretize_portico(start_node, end_node, delta_x):
-    x_start, y_start = start_node
-    x_end, y_end = end_node
-
-    length = np.sqrt((x_end - x_start) ** 2 + (y_end - y_start) ** 2)
-    num_elements = int(length / delta_x)
-
-    node_list = []
-
-    dx = (x_end - x_start) / num_elements
-    dy = (y_end - y_start) / num_elements
-
-    for i in range(num_elements + 1):
-        x = x_start + i * dx
-        y = y_start + i * dy
-        node_list.append([x, y])
-
-    return np.array(node_list)
 
 
-def generate_global_matrix(matrixes_list):
-    global_matrix = pd.concat(matrixes_list).groupby(level=0).sum()
-    return global_matrix
+
+
+
+
 
 class Portico():
+    
+    def __init__(self, delta, start_node, end_node, theta, element_name):
+        
+        self.delta = delta
+        self.stiffness_matrix_list = self.generate_local_stiffness_matrix()
+        self.start_node = start_node
+        self.end_node = end_node
+        self.E = 210   # Parâmetro do ACO SAE 1045
+        self.theta = theta
+        self.element_name = element_name
+    
+    def generate_stiffness_matrix(self):
+        # Discretiza o dominio de acordo com delta, gerando listas de nos que serao feitas
+        nodes_list = self.discretize_portico(self.start_node, self.end_node)
+        
+        local_matrixes = []
+        # Com a lista de nos, gera uma matriz local para cada elemento discretizado
+        for element_nodes in nodes_list:
+            
+            # Calcula parametros que vao mudar com o elemento
+            I, A = self.identify_element(self.element_name)
+                  
+            # Gera a matriz local e rotaciona de acordo com a necessidade
+            stiff_matrix = self.rigidez_portico_matrix(self.E, A, self.delta, I)
+            rotation_matrix = self.rotate_portico_matrix(self.theta)
+            
+            stiffness_matrix = np.matmul(np.matmul(np.transpose(
+                rotation_matrix), stiff_matrix), rotation_matrix)
+            
+            df_matrix = self.generate_dataframe_from_matrix(stiffness_matrix, element_nodes)
+            local_matrixes.append(df_matrix)
 
-stiff_1 = rigidez_trelica_matrix(E=5, A=4, L=4)
-t_inclination_1 = trelica_rotation_matrix(60)
-rotated_1 = np.matmul(np.matmul(np.transpose(
-    t_inclination_1), stiff_1), t_inclination_1)
+        # Usa todas as matrizes locais e gera uma global para o elemento de portico
+        global_matrix = self.generate_global_matrix(local_matrixes)
+        return global_matrix
 
+    def generate_global_matrix(matrixes_list):
+        global_matrix = pd.concat(matrixes_list).groupby(level=0).sum()
+        return global_matrix
+    def generate_dataframe_from_matrix(self, local_matrix, nodes):
 
-stiff_2 = rigidez_portico_matrix(E=3, A=2, L=2, I=0.1)
-t_inclination_2 = portico_rotation_matrix(0)
+        column_names = []
+        index_names = []
+        for node in nodes:
+            u_dof = f"{node}_u"
+            v_dof = f"{node}_v"
+            phi_dof = f"{node}_phi"
 
-rotated_2 = np.matmul(np.matmul(np.transpose(
-    t_inclination_2), stiff_2), t_inclination_2)
+            column_names += [u_dof, v_dof, phi_dof]
+            index_names += [u_dof, v_dof, phi_dof]
 
+        matrix_df = pd.DataFrame(
+            data=local_matrix, columns=column_names, index=index_names)
 
-df1 = generate_dataframe_from_matrix(rotated_2, [0, 1])
-df2 = generate_dataframe_from_matrix(rotated_1, [1, 7])
+        return matrix_df
+    
+    def rotate_portico_matrix(theta):
+        T = np.zeros(shape=(6, 6))
+        theta = np.radians(theta)
+        T[0, :] = [np.cos(theta), np.sin(theta), 0, 0, 0, 0]
+        T[1, :] = [- np.sin(theta), np.cos(theta), 0, 0, 0, 0]
+        T[2, 2] = 1
+        T[3, :] = [0, 0, 0, np.cos(theta), np.sin(theta), 0]
+        T[4, :] = [0, 0, 0, -np.sin(theta), np.cos(theta), 0]
+        T[5, 5] = 1
+
+        return T
+
+    def rigidez_portico_matrix(self, E, A, L, I):
+        K = np.zeros(shape=(6, 6))
+        K[0, 0] = 1
+        K[0, 3] = -1
+        K[3, 0] = -1
+        K[3, 3] = 1
+
+        K *= (E * A) / L
+
+        K[1, :] = np.array([0, 12, 6*L, 0, -12, 6*L]) * ((E * I)/(L**3))
+
+        K[2, :] = np.array([0, 6*L, 4*(L**2), 0, -6*L, 2*(L**2)]
+                           ) * ((E * I)/(L**3))
+
+        K[4, :] = np.array([0, -12, -6*L, 0, 12, 6*L]) * ((E * I)/(L**3))
+
+        K[5, :] = np.array([0, 6*L, 2*(L**2), 0, -6*L,
+                            4*(L**2)]) * ((E * I)/(L**3))
+
+        return K
+
+    def discretize_portico(self, start_node, end_node):
+        x_start, y_start = start_node
+        x_end, y_end = end_node
+
+        length = np.sqrt((x_end - x_start) ** 2 + (y_end - y_start) ** 2)
+        num_elements = int(length / self.delta)
+
+        node_list = []
+
+        dx = (x_end - x_start) / num_elements
+        dy = (y_end - y_start) / num_elements
+
+        for i in range(num_elements + 1):
+            x = x_start + i * dx
+            y = y_start + i * dy
+            node_list.append([x, y])
+
+        return np.array(node_list)
+
